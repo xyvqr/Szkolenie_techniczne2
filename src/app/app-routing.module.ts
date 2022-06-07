e23f41ca61dfdcab98a8801a2d4f93c6132a2f8f
Michał Kycia <michalkycia07@wp.pl>
2022-06-08 01:25:56 +0200

    contact/about-us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
-import {PriceComponent} from "./price/price.component";
-import {RentComponent} from "./rent/rent.component";
+import {ONasComponent} from "./o-nas/o-nas.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'price', component: PriceComponent},
    {path: 'contact', component:ContactComponent },
-   {path: 'rent', component: RentComponent },
+   {path: 'o-nas', component:ONasComponent },
    {path: 'login', component: LoginComponent },
    {path: 'register', component: RegisterComponent },
   {path: '**', component: HomeComponent, pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,21 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
-import { RentComponent } from './rent/rent.component';
-import { PriceComponent } from './price/price.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { ONasComponent } from './o-nas/o-nas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    RentComponent,
-    PriceComponent,
     ContactComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ONasComponent
   ],
   imports: [
     BrowserModule,
